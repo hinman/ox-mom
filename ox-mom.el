27c358889375904bb2d79a89136cb6b93d59d909
Lee Hinman <hinman@gmail.com>
2020-04-19 22:10:37 -0500

    get src-code blocks working

--- ox-mom.el
+++ ox-mom.el
@@ -36,13 +36,52 @@
 (org-export-define-backend
  'mom
  '((bold . org-mom-bold)
+   (center-block . org-mom-not-implemented)
+   (clock . org-mom-not-implemented)
+   (code . org-mom-not-implemented)
+   (drawer . org-mom-not-implemented)
+   (dynamic-block . org-mom-not-implemented)
    (entity . org-mom-entity)
+   (example-block . org-mom-not-implemented)
+   (export-block . org-mom-not-implemented)
+   (export-snippet . org-mom-not-implemnted)
+   (fixed-width . org-mom-not-implemnted)
+   (footnote-definition . org-mom-not-implemnted)
+   (footnote-reference . org-mom-not-implemnted)
    (headline . org-mom-headline)
+   (horizontal-rule . org-mom-not-implemnted)
    (inline-src-block . org-mom-inline-src-block)
+   (inlinetask . org-mom-not-implemnted)
+   (italic . org-mom-not-implemnted)
+   (item . org-mom-not-implemnted)
+   (keyword . org-mom-not-implemnted)
+   (line-break . org-mom-not-implemnted)
+   (link . org-mom-not-implemnted)
+   (node-property . org-mom-not-implemnted)
    (paragraph . org-mom-paragraph)
+   (plain-list . org-mom-not-implemnted)
    (plain-text . org-mom-plain-text)
+   (planning . org-mom-not-implemnted)
+   (property-drawer . org-mom-not-implemnted)
+   (quote-block . org-mom-not-implemnted)
+   (radio-target . org-mom-not-implemnted)
    (section . org-mom-section)
-   (template . org-mom-template))
+   (special-block . org-mom-not-implemnted)
+   (src-block . org-mom-src-block)
+   (statistics-cookie . org-mom-not-implemnted)
+   (strike-through . org-mom-not-implemnted)
+   (subscript . org-mom-not-implemnted)
+   (superscript . org-mom-not-implemnted)
+   (table . org-mom-not-implemnted)
+   (table-cell . org-mom-not-implemnted)
+   (table-row . org-mom-not-implemnted)
+   (target . org-mom-not-implemnted)
+   (template . org-mom-template)
+   (timestamp . org-mom-not-implemnted)
+   (underline . org-mom-not-implemnted)
+   (verbatim . org-mom-not-implemnted)
+   (verse-block . org-mom-not-implemnted))
+
  :menu-entry
  '(?m "Export to MOM"
       ((?m "As MOM file" org-mom-export-to-mom))))
@@ -101,6 +140,12 @@ contextual information."
 (defun org-mom-inline-src-block (inline-src-block contents info)
   (format "%s" contents))
 
+;;; Not-implemented
+(defun org-mom-not-implemented (text contents info)
+  (format "text %s\n" text)
+  (format "contents %s\n" contents)
+  (format "info %s\n" info))
+
 ;;; Plain Text
 (defun org-mom-plain-text (text info)
   (format "%s" text))
@@ -113,6 +158,18 @@ contextual information."
 (defun org-mom-section (section contents info)
   (format "%s" contents))
 
+;;; src-block
+(defun org-mom-src-block (src-block contents info)
+  "Transcode a SRC-BLOCK element from Org to Mom Groff
+Contents holds the contenst of the item.  INFO is a plist
+holding contextual information."
+  (let* ((lang (org-element-property :language src-block))
+         (label (org-element-property :name src-block))
+         (code (org-element-property :value src-block))
+         (num-start (org-export-get-loc src-block info))
+         (retain-labels (org-element-property :retain-labels src-block)))
+  (format ".QUOTE\n.CODE\n%s.QUOTE OFF\n"
+	  (org-export-format-code-default src-block info))))
 
 ;;; Interactive functions
 
